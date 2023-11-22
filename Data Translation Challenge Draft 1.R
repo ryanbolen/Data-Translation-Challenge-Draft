8b33b1fe9a411f604992adb0c89183c7db941c2f
ryanbolen <rbolen@seattlu.edu>
2023-11-21 17:17:15 -0800

    Initial commit project setup.

---
+++ Data Translation Challenge Draft 1.R
@@ -0,0 +1,13 @@
+# The ACME corporation is considering moving into agricultural inputs in Ghana.
+# To get an idea of whom to target for their sales efforts they have hired you to analyze what determines agricultural profit there.
+# They are especially interested in what effect household educational attainment and the characteristics of the local area has on profit,
+# but if you find other results of interest, they would like to hear those as well.
+# They have left the choice of explanatory variables and variable definitions to you,
+# but to be able to compare across regions they want  
+
+# Requirements(supposed):
+# Analyze what determines agricultural profit there in order to determine who to target sale efforts towards.
+# Interested in what effect household educational attainment and the characteristics of the local area has on profit.
+# Must compare across regions.
+# At least one specification that examines profit per area unit (for example, acre, hectare, or square meters).
+# If you find other results of interest, they would like to hear those as well.
